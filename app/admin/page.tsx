dc8c26dc7aa5ff8f12ea3fbe9c4d31d05f1a7c5e
unknown <hrishikeshblaster@gmail.com>
2024-11-30 23:54:59 +0530

    fixed errors

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,65 +1,9 @@
-"use client"
+import React from 'react'
 
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import toast from 'react-hot-toast'
-
-const Admin = () => {
-  
-  const [messages, setMessages]=useState([])
-  const [loading, setLoading]=useState(false)
-
-  useEffect(()=>{
-    (async function(){
-      try {
-      setLoading(true)
-      const data=await axios.get('/api/get-message')
-      if(data.status===200){
-        setMessages(data.data.data)
-        setLoading(false)
-      }
-      
-      } catch (error) {
-        toast.error("Something went wrong!")
-        console.log(error)
-        setMessages([])
-      }
-
-    })()
-  },[])
-
+const page = () => {
   return (
-    <main className='mt-[3em] w-full h-auto py-20 px-10 flex flex-col items-center justify-center max-sm:block  box-border' >
-      {loading? <h2 className='text-white font-bold Welcome-text'>Loading...</h2>:       
-      <table className='w-full bg-white bg-opacity-50 rounded'>
-       <thead>
-        <tr >
-          <th className='p-2'>Sl No</th>
-          <th className='p-2'>Name</th>
-          <th className='p-2'>Email</th>
-          <th className='p-2'>Message</th>
-        </tr>
-       </thead>
-       <tbody>
-        {messages.map((eachMessage:any,index:number)=> {
-          return (
-        <tr key={eachMessage?.id}>
-          <td className='text-center border-t-2 border-black p-4 font-semibold'>{index+1}</td>
-          <td className='text-center border-t-2 border-black p-4 font-semibold'>{eachMessage?.name}</td>
-          <td className='text-center border-t-2 border-black p-4 font-semibold'>{eachMessage?.email}</td>
-          <td className='text-center border-t-2 border-black p-4 font-semibold text-wrap'>{eachMessage?.message}</td> 
-        </tr>
-          )
-        })}
-
-       </tbody>
-
-
- 
-      </table>}
-
-    </main>
+    <div>page</div>
   )
 }
 
-export default Admin
+export default page
--- components/sub/ContactForm.tsx
+++ components/sub/ContactForm.tsx
@@ -6,79 +6,79 @@ import toast from 'react-hot-toast';
 
 const ContactForm = () => {
 
-  const [btnDisabled, setBtnDisabled]=useState(false)
-  const [values, setValues]=useState({
-      name:'',
-      email:'',
-      message:''
+  const [btnDisabled, setBtnDisabled] = useState(false)
+  const [values, setValues] = useState({
+    name: '',
+    email: '',
+    message: ''
   })
 
 
-  const formik=useFormik({
-    initialValues:values,
-    onSubmit: async(values)=>{
-      try {
-      setBtnDisabled(true)
-      const data=await axios.post('/api/save-message',values)
-      console.log(data)
-      if(data.status===200)
-          setBtnDisabled(false)
-          setValues({name:'', email:'', message:''})
-          toast.success("Message sent succesfully!")
-      } catch (error) {
-        toast.error("Something went wrong! Please try again later.")
-      }
- 
+  const formik = useFormik({
+    initialValues: values,
+    onSubmit: async (values) => {
+      // try {
+      //   setBtnDisabled(true)
+      //   const data = await axios.post('/api/save-message', values)
+      //   console.log(data)
+      //   if (data.status === 200)
+      //     setBtnDisabled(false)
+      //   setValues({ name: '', email: '', message: '' })
+      //   toast.success("Message sent succesfully!")
+      // } catch (error) {
+      //   toast.error("Something went wrong! Please try again later.")
+      // }
+
     },
     validationSchema: yup.object({
-      name:yup.string().trim().required('Name is Required'),
+      name: yup.string().trim().required('Name is Required'),
       email: yup.string().email('Must be a valid email').required('Email is required'),
-      message:yup.string().trim().required('Message is required'),
+      message: yup.string().trim().required('Message is required'),
     })
   })
 
-  
+
 
   return (
     <div className='bg-[#000] bg-opacity-50 rounded-lg px-7 py-20 z-40 flex flex-col items-center justify-center w-[70%] max-sm:w-[100%]'>
-                <form className='w-full flex flex-col justify-center items-center' onSubmit={formik.handleSubmit}>
-      <input className='bg-transparent border-b-2 border-b-white w-[70%] outline-none text-white mb-10 z-40 select-none' 
-             placeholder='Name'
-             type='text' 
-             name='name'              
-             onChange={formik.handleChange}
-             onBlur={formik.handleBlur}
-             value={formik.values.name}/>
-                 {formik.errors.name && (
-                    <div className=" text-red-600 mb-10">{formik.errors.name}</div>
-                )}
-      <input className='bg-transparent border-b-2 border-b-white w-[70%] outline-none text-white mb-10' 
-             placeholder='Email' 
-             type='email'
-             name='email'              
-             onChange={formik.handleChange}
-             onBlur={formik.handleBlur}
-             value={formik.values.email}
-             />
-                 {formik.errors.email && (
-      <div className="text-red-600 mb-10">{formik.errors.email}</div>
-    )}
-      <textarea className='bg-transparent border-2 border-white w-[70%] outline-none text-white mb-10 h-[250px] p-2 overflow-auto resize-none' 
-                placeholder='Write me a message'
-                name='message'              
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.message}
-                ></textarea>
-                    {formik.errors.message && (
-      <div className="text-red-600 mb-10">{formik.errors.message}</div>
-    )}
-      <button className='button-64' role='button' type="submit" disabled={btnDisabled}><span className="text"> {btnDisabled? "Please Wait...":"Send Message"}</span></button>
-          </form>
+      <form className='w-full flex flex-col justify-center items-center' >
+        <input className='bg-transparent border-b-2 border-b-white w-[70%] outline-none text-white mb-10 z-40 select-none'
+          placeholder='Name'
+          type='text'
+          name='name'
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.name} />
+        {formik.errors.name && (
+          <div className=" text-red-600 mb-10">{formik.errors.name}</div>
+        )}
+        <input className='bg-transparent border-b-2 border-b-white w-[70%] outline-none text-white mb-10'
+          placeholder='Email'
+          type='email'
+          name='email'
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.email}
+        />
+        {formik.errors.email && (
+          <div className="text-red-600 mb-10">{formik.errors.email}</div>
+        )}
+        <textarea className='bg-transparent border-2 border-white w-[70%] outline-none text-white mb-10 h-[250px] p-2 overflow-auto resize-none'
+          placeholder='Write me a message'
+          name='message'
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.message}
+        ></textarea>
+        {formik.errors.message && (
+          <div className="text-red-600 mb-10">{formik.errors.message}</div>
+        )}
+        <button className='button-64' role='button' type="submit" disabled={btnDisabled}><span className="text"> {btnDisabled ? "Please Wait..." : "Send Message"}</span></button>
+      </form>
 
-   
-      
-      
+
+
+
       <p className='text-[0.8rem] text-[#eee]'>or email me at: <a className='cursor-pointer underline font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500' href='mailto:hrishikeshghoshofficial@gmail.com'>hrishikeshghoshofficial@gmail.com</a></p>
     </div>
   )
